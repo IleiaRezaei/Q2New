3c20ce8506bcbec5e83abb943810a43e66c04b26
MUKILTEO\1710247 <ethanlmcpeek@gmail.com>
2022-01-11 12:56:00 -0800

    Parallax

--- Assets/EthanM/Scripts/Paralax1.cs
+++ Assets/EthanM/Scripts/Paralax1.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Paralax1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
     void Start()
     {
-        
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
         
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+
     }
 }
