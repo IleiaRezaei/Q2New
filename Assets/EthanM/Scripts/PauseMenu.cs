75da16df9bc503d074a04a8d9db0cdb00cd10d1d
MUKILTEO\1710247 <ethanlmcpeek@gmail.com>
2022-01-12 12:22:11 -0800

    PauseMenuAdded

---
+++ Assets/EthanM/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool pausemenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if(pausemenu == false)
+            {
+                openPausemenu();
+                pausemenu = true;
+            }
+            else
+            {
+                Closepausemenu();
+                pausemenu = false;
+            }
+
+        }
+    }
+    void openPausemenu()
+    {
+        Time.timeScale = 0;
+    }
+    
+    void Closepausemenu()
+    {
+        Time.timeScale = 1;
+    }
+} 
