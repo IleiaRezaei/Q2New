d082775f6716f6efb673b4f6af9a3b328645f33e
MUKILTEO\1710197 <ileia.rezaei2005@gmail.com>
2022-01-13 11:31:23 -0800

    a

--- Assets/IleiaR/Scripts/RedPotatoExplosion.cs
+++ Assets/IleiaR/Scripts/RedPotatoExplosion.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class RedPotatoExplosion : MonoBehaviour
 {
+    public int explosionDamage = 100;
+    public bool isExploding;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+
+    private Rigidbody2D RigidBoy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        RigidBoy.GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isExploding == true)
+        {
+            anim.SetBool("Explode", true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            isExploding = true;
+        }
     }
 }
