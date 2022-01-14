417dcddc8d59ad3037a5e8b85c4be18244734806
MUKILTEO\1710247 <ethanlmcpeek@gmail.com>
2022-01-14 13:25:10 -0800

    TeleportScene

---
+++ Assets/IleiaR/Scripts/nextGameScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextGameScene : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("RadiatedScene");
+        }
+    }
+}
