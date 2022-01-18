511e4cfca74fa5c5e0e7a6aa99ad6e56fc2cfddb
MUKILTEO\1710247 <ethanlmcpeek@gmail.com>
2022-01-18 12:24:07 -0800

    no message

--- Assets/IleiaR/Scripts/SeneStart.cs
+++ Assets/IleiaR/Scripts/SeneStart.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 
 public class SeneStart : MonoBehaviour
 {
+    public bool pausemenu;
+    public GameObject pausemenumenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<PauseMenu>().Closepausemenu();
-        Time.timeScale = 1;
+       
+        
+            if (pausemenu == false)
+            {
+                openPausemenu();
+                pausemenu = true;
+            }
+            else if (pausemenu == true)
+            {
+                Closepausemenu();
+                pausemenu = false;
+            }
+
+        void openPausemenu()
+        {
+            Time.timeScale = 0;
+            pausemenumenu.SetActive(true);
+        }
+
+        void Closepausemenu()
+        {
+            pausemenumenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+
     }
 
 }
