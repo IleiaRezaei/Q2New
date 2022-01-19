50e3ef041f1b92aa79bff2fbb7bbefd85bb172a9
MUKILTEO\1710197 <ileia.rezaei2005@gmail.com>
2022-01-19 13:20:45 -0800

    Red potato Done

--- Assets/IleiaR/Scripts/RedPotatoExplosion.cs
+++ Assets/IleiaR/Scripts/RedPotatoExplosion.cs
@@ -13,16 +13,32 @@ public class RedPotatoExplosion : MonoBehaviour
 
     private Rigidbody2D RigidBoy;
 
+    public bool held;
+
+    public GameObject child;
+    public GameObject me;
+
+    private IEnumerator coroutine;
+
+    public int waitFor;
+
+    public bool die = false;
+
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        RigidBoy.GetComponent<Rigidbody2D>();
+        RigidBoy = GetComponent<Rigidbody2D>();
 
         animRedPot = GetComponent<Animator>();
 
         isExploding = false;
         //anim_RedPot.SetBool("RedExplode", false);
+        StartCoroutine(waitForSeconds());
+        StartCoroutine(waituntilExplode());
+
+
+
     }
 
     // Update is called once per frame
@@ -33,8 +49,14 @@ public class RedPotatoExplosion : MonoBehaviour
             //anim_RedPot.Play("Explosion");
             //animRedPot.SetBool("Explode", true);
             animRedPot.SetBool("ex", true);
+            animRedPot.SetBool("hasWaited", true);
+            GetComponent<Rigidbody2D>().freezeRotation = true;
         }
-
+        if (die)
+        {
+            //waituntilExplode();
+            Destroy(me);
+        }
         //Debug.Log(isExploding);
     }
 
@@ -46,12 +68,50 @@ public class RedPotatoExplosion : MonoBehaviour
             Debug.Log("Hi---------");
 
             isExploding = true;
-            animRedPot.SetBool("Explode", true);
+            //animRedPot.SetBool("Explode", true);
             //anim_RedPot.SetBool("Explode", true);
             //animRedPot.Play("Explosion");
             //animRedPot.SetBool("isAttacking", false);
 
+            //yield return new WaitForSeconds(5);
+            //Debug.Log("))))))))))((((((((((((((((())))))))))))))((((((((((((((((((((ANIMATIONPLAYED");
+            waitForSeconds();
+            //waituntilExplode();
+        }
+
+
+        if (collision.gameObject.tag == "PlayerAttack")
+        {
+            if(held == false) {
+                if (collision.gameObject.transform.parent.gameObject.GetComponent<CharacterControll>().holding != true)
+                {
+                    RigidBoy.angularVelocity = 0;
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                    held = true;
+                    gameObject.layer = 9;
+                    GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                    GetComponent<Rigidbody2D>().freezeRotation = false;
+                    RigidBoy.drag = 5;
+                    RigidBoy.angularDrag = 0.5F;
+                    collision.gameObject.transform.parent.gameObject.GetComponent<CharacterControll>().heldobj = this.gameObject;
+                    collision.gameObject.transform.parent.gameObject.GetComponent<CharacterControll>().holding = true;
+                }
+            }
+        }
+        else
+        {
+            //tag = "Untagged";
         }
     }
+    IEnumerator waitForSeconds()
+    {
+        yield return new WaitForSeconds((int)4.5);
+        child.SetActive(true);
+    }
 
+    IEnumerator waituntilExplode()
+    {
+        yield return new WaitForSeconds(5);
+        
+    }
 }
