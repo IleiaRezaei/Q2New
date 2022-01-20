9c8a5db7197c74824e2498859c599432425bc65e
DESKTOP-T1NG7IC\ileia_ <ileia.rezaei2005@gmail.com>
2022-01-19 23:53:34 -0800

    e

---
+++ Assets/LoadCreds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadCreds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(loadSceneAfterDelay(60));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator loadSceneAfterDelay(float waitbySecs)
+    {
+
+        yield return new WaitForSeconds(waitbySecs);
+        SceneManager.LoadScene("CreditScene");
+    }
+}
